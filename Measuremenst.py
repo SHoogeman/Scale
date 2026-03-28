3b68d187cb9e8419e396b6d85033cf65f23ceb8d
MrBBrightside <s.t.hoogeman@student.tudelft.nl>
2026-03-28 18:35:14 +0100

    push

--- Measuremenst.py
+++ Measuremenst.py
@@ -2,10 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os 
 
-path = os.getcwd() + "\Scale\Data\DATALOG.TXT"
+path = os.getcwd() 
+data_path = path + r"\Scale\Data\DATALOG.TXT"
+save_path = path + r"\Scale\Plots"
 
-
-data = np.loadtxt(path, delimiter=',', skiprows=0, dtype=str)
+data = np.loadtxt(data_path, delimiter=',', skiprows=0, dtype=str)
 
 Time = data[:,0]
 Pressure = data[:,1]
@@ -27,5 +28,7 @@ Mass_filtered = np.convolve(filter, Mass, "same")
 
 fig, ax = plt.subplots(2)
 ax[0].plot(t, Flow, label="flow")
-ax[1].plot(np.linspace(0,dt*len(Time), len(Time)), Mass_filtered)
+ax[1].plot(np.linspace(0, dt*len(Time), len(Time)), Mass_filtered)
+#ax[0].set_labels(xlabel= r"$\Phi_m$", ylabel = )
+plt.savefig(save_path + "\MassflowAndWeight", dpi=800)
 plt.show()
