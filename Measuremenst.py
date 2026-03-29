c701bd6fe5615ffa92a752070034fb586c6c77b0
shoog <s.t.hoogeman@tudelf.nl>
2026-03-29 11:04:43 +0200

    tried some more things

--- Measuremenst.py
+++ Measuremenst.py
@@ -9,9 +9,9 @@ save_path = path + r"\Scale\Plots"
 data = np.loadtxt(data_path, delimiter=',', skiprows=0, dtype=str)
 
 Time = data[:,0]
-Pressure = data[:,1]
-Temperature = data[:,2]
-Humidity = data[:,3]
+Pressure = data[:,1].astype(float)
+Temperature = data[:,2].astype(float)
+Humidity = data[:,3].astype(float)
 Mass = data[:,4].astype(float)
 
 
@@ -26,9 +26,18 @@ filter = np.ones(15)
 Flow = np.convolve(filter, Mass_flow, "same")
 Mass_filtered = np.convolve(filter, Mass, "same")
 
-fig, ax = plt.subplots(2)
+fig, ax = plt.subplots(5)
 ax[0].plot(t, Flow, label="flow")
 ax[1].plot(np.linspace(0, dt*len(Time), len(Time)), Mass_filtered)
-#ax[0].set_labels(xlabel= r"$\Phi_m$", ylabel = )
+ax[2].plot(np.linspace(0, dt*len(Temperature), len(Temperature)), Temperature)
+ax[3].plot(np.linspace(0, dt*len(Temperature), len(Temperature)), Humidity)
+ax[4].plot(np.linspace(0, dt*len(Temperature), len(Temperature)), Pressure)
 plt.savefig(save_path + "\MassflowAndWeight", dpi=800)
 plt.show()
+
+#cor_hum_Mass_flow = np.correlate(Mass_flow, Humidity, "same")
+
+#fig, axs = plt.subplots(1)
+#axs.plot(np.linspace(0, dt*len(cor_hum_Mass_flow), len(cor_hum_Mass_flow)), cor_hum_Mass_flow)
+#plt.savefig(save_path + "\correlation", dpi=800)
+
