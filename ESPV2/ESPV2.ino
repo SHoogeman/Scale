85745bcca59e4cea93d415627e1c41854e0dd153
MrBBrightside <s.t.hoogeman@student.tudelft.nl>
2026-04-06 11:07:22 +0200

    ESP logger works

--- ESPV2/ESPV2.ino
+++ ESPV2/ESPV2.ino
@@ -2,14 +2,25 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClientSecureBearSSL.h>
+#include <SoftwareSerial.h>
 
  
 // Replace with your WiFi and Web App details
 //const char* ssid = "📶WirelessFirmware📶";
 //const char* password = "ElektroCyberSecurityKonijn";
-const char* scriptURL = "https://script.google.com/macros/s/AKfycbymWAoic5WTgZoxp1sjdReDGKZ3s_Arxp831kt8tEvcVZpcdE2lo33ol5yg4g38s-jPCw/exec";
-const char* ssid = "S21 van Sjoerd";
-const char* password = "ugki4500";
+const char* scriptURL = "https://script.google.com/macros/s/AKfycbweea9BWjkBLszlII4J5-T4UEJ6stplfIiWQCkz4vzkXJwXTtZEKKcMP3T76XCkXbnQyA/exec";
+//const char* ssid = "S21 van Sjoerd";
+//const char* password = "ugki4500";
+const char* ssid = "XTRA-Gast";
+const char* password = "Welkom123";
+
+SoftwareSerial arduino(D7, D6); //The object to receive data from arduino
+
+String inputString = "";         // A String to hold incoming data
+
+bool stringComplete = false;      // Whether the string is complete
+
+int counter = 0;
  
 void setup() {
   Serial.begin(115200);
@@ -19,33 +30,88 @@ void setup() {
     Serial.println("Connecting...");
   }
   Serial.println("Connected to WiFi");
+
+  arduino.begin(115200); //init software serial 
+  pinMode(D7, INPUT);
+  pinMode(D6, OUTPUT);
+  inputString.reserve(200); // Reserve 200 bytes for the string
+
 }
  
 void loop() {
+  while (arduino.available() && stringComplete == false) {
+    char inChar = arduino.read();
+       if (inChar == '\n') {
+      stringComplete = true;
+    } else if (inChar != '\r') {
+      inputString += inChar;
+    }
+  }
+  if (stringComplete) {
+
+    String time = "";
+    String pres = "";
+    String temp = "";
+    String hum = "";
+    String mass = "";
+    Serial.println(inputString);
+
+    for (int i = 0; i < inputString.length(); i++){
+      if (String(inputString.charAt(i)) == ",") {
+        counter += 1;
+      }
+       else {
+         switch(counter){
+           case 0:{
+             time += char(inputString[i]);
+           }
+           break;
+           case 1:{
+             pres += char(inputString[i]);
+           }
+           break;
+           case 2:{
+             temp += char(inputString[i]);
+           }
+           break;
+           case 3:{
+             hum += char(inputString[i]);
+           }
+          break;
+          case 4:{
+            mass += char(inputString[i]);
+          }
+          break;
+
+          default: {
+          }    
+        }
+      }     
+    }
+    // Clear the string for the next message
+    stringComplete = false;
+    sendData(temp, hum, time, pres, mass);
+    counter = 0; 
+    inputString="";
+  }
+  delay(100);
+}
+
+
+
+void sendData(String temp, String hum, String time, String pres, String mass) {
+  
   if (WiFi.status() == WL_CONNECTED) {
+
     std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
     client->setInsecure();
-
 
     HTTPClient http;
 
     http.begin(*client, scriptURL);
     http.addHeader("Content-Type", "application/json");
- 
-    // Replace with your sensor data
-    float temp = random(20, 30);  // Simulated temperature
-    float hum = random(40, 60);   // Simulated humidity
-    float time = random(40, 60);
-    float pres = random(40, 60);
-    float masss = random(40, 60);
-    
-    //String jsonData = "{\"time\": " + String(Time) + ", \"temp\": " + String(temp) + ", \"hum\": " + String(hum) + ", \"pres\": " + String(pres) + ", \"mass\": " + String(mass) +"}";
- 
-    //float temp = random(20, 30);  // Simulated temperature
-    //float hum = random(40, 60);   // Simulated humidity
- 
-    String jsonData = "{\"temp\": " + String(temp) + ", \"hum\": " + String(hum) + ", \"time\": " + String(time) + ", \"pres\": " + String(pres) + "}";
-
+     
+    String jsonData = "{\"temp\": " + temp + ", \"hum\": " + hum + ", \"time\": " + time + ", \"pres\": " + pres + ", \"mass\": " + mass + "}";
 
     int httpResponseCode = http.POST(jsonData);
  
@@ -54,6 +120,5 @@ void loop() {
   } else {
     Serial.println("WiFi Disconnected");
   }
- 
-  delay(10000); // Send data every 10 seconds
+  delay(200);
 }
